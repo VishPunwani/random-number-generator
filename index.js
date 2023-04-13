3ebaa8ca7b144849a817c6cf5806d68603171ecb
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-04-12 19:31:17 -0700

    working on javascript for button

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const button = document.querySelector("#btn");
 const getNumber = Math.random();
 
+button.addEventListener("click", myFunction);
+
+
+button.addEventListener('click', () => {
+    
+    button.style.display = 'none';
+  
+    
+    const box = document.getElementById('#box');
+    box.style.display = 'block';
+  });
+  
+function myFunction(){
+    alert(getNumber)
+}
+
+
+
 
 
