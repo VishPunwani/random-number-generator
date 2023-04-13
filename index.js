113f2a754565530c0490d40cd3f6c027d6f675f5
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-04-12 21:14:54 -0700

    adding another html page

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const button = document.querySelector("#btn");
 const getNumber = Math.random();
 const reset  = document.querySelector("#reset");
 
+
 // testing code commented out
 //button.addEventListener("click", myFunction);
 
 //function myFunction(){
-    //alert(getNumber)
+  //  alert(getNumber)
 //}
 
 
-button.addEventListener('click', () => {
-    
-    button.style.display = 'none';
 
+
+button.addEventListener('click', () => {
+    button.style.display = 'none';
     const box = document.getElementById('#box');
     box.style.display = 'block';
   });
   
 
   reset.addEventListener('click', () => {
-    
     button.style.display = '';
-
     const box = document.getElementById('#box');
     box.style.display = 'block';
   });
+  
   
 
 
