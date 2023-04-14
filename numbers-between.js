2ed98a03273b2e5afe06baec8a1af87afa29ff20
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-04-13 21:56:16 -0700

    still working on display

--- numbers-between.js
+++ numbers-between.js
@@ -1,17 +1,21 @@
 
 const button = document.querySelector("#btn");
-let min = parseInt(document.getElementById('#min')). value;
-let max = parseInt(document.getElementById('#max')). value;
 let output = document.querySelector('#output');
 const reset  = document.querySelector("#reset");
-const equation = Math.floor(Math.random()*(max-min+1))+min;
+const equation = Math.floor(Math.random()* 100)
 
 
 
+function getValue(){
+  var x = document.getElementById('#min')
+  //var y = document.getElementById('#max')
+  document.getElementById("#output").innerHTML = x;
+
+}
 
 
-button.addEventListener("click", ()=>{
-output.innerHTML =  Math.floor(Math.random()*(max-min+1))+min;
+button.addEventListener("click", (getValue)=>{
+output.innerHTML = getValue;
 })
 
 
